3dc29352b225448dff728bb655aa2fd137d973b5
DSBD <baciudavid.dragos@gmail.com>
2019-03-12 12:43:06 -0400

    Fixing Python Environment

--- python_env/django_project/django_project/settings.py
+++ python_env/django_project/django_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'helloapp.apps.HelloappConfig'
+    'helloapp.apps.HelloappConfig',
 ]
 
 MIDDLEWARE = [
