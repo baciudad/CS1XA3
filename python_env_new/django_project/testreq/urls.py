58d1a6265b816b9d38617a5c514c5b50b3209ffe
DSBD <baciudad@mcmaster.ca>
2019-03-14 16:49:49 +0000

    More Curtis Lab Test Code

--- python_env_new/django_project/testreq/urls.py
+++ python_env_new/django_project/testreq/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("post/", views.posttest, name = "testreq-posttest"),
+    path("get/", views.gettest, name = "testreq-gettest"),
     path("", views.hello, name = "testreq-hello"),
 ]
--- python_env_new/django_project/testreq/views.py
+++ python_env_new/django_project/testreq/views.py
@@ -3,3 +3,15 @@ from django.http import HttpResponse
 
 def hello(request):
     return HttpResponse("Hello")
+
+def gettest(request):
+    keys = request.GET
+    name = keys.get("name", default="")
+    age = keys.get("age", default="")
+    return HttpResponse("Hello " + name + " your " + age + " years old!")
+
+def posttest(request):
+    keys = request.POST
+    name = keys.get("name", default="")
+    age = keys.get("age", default="")
+    return HttpResponse("Hello " + name + " your " + age + " years old!\n")   
